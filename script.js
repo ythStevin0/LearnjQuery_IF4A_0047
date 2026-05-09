27b58a90e2b659b67b5533e1a9dd336779c5aeff
ythStevin0 <404Stevino@gmail.com>
2026-05-10 03:36:30 +0700

    refactor: merubah tampilan button edit dan hapus menjadi icon

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ if (inputValue.value === "" || inputDate.value === "") {
     listBaru.appendChild(teksTugas);
 
     const btnEdit = document.createElement("button");
-    btnEdit.innerHTML = "Edit";
+    btnEdit.innerHTML = `<svg viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>`;
     btnEdit.className = "btnEdit";
+    btnEdit.title = "Edit Tugas";
 
     btnEdit.onclick = function() {
         const tugasBaru = prompt("Edit Tugas:", spanTugas.innerHTML);
@@ -42,8 +43,9 @@ if (inputValue.value === "" || inputDate.value === "") {
     };
 
     const btnHapus = document.createElement("button");
-    btnHapus.innerHTML = "Hapus";
+    btnHapus.innerHTML = `<svg viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>`;
     btnHapus.className = "btnDelete";
+    btnHapus.title = "Hapus Tugas";
     
     btnHapus.onclick = function() {
         if(confirm("Apakah Anda yakin ingin menghapus tugas ini?")) {
